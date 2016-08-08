4463af9bc84eb376b6be7f4fc8ad5fbcc71e2075
shengsheng <1291367489@qq.com>
2016-08-08 15:21:51 +0800

    增加个人信息界面
    Signed-off-by: shengsheng <1291367489@qq.com>

--- website/js/info.js
+++ website/js/info.js
@@ -28,4 +28,44 @@
 	  		$(".info-basic").slideUp();
 		}
   });
- });
+
+// 背景移动所需参数
+	$sky1 = $(".section");
+	$sky2 = $("#sky");
+	mainwidth = $(".section").outerWidth();  
+	offset = -$(".section").width();
+
+	var offset1 = 0;//显示的云
+	var offset2 = offset;//隐藏的云
+ //背景云移动
+ setInterval(function flutter() {
+	if (offset1 >= mainwidth) {
+		offset1 =  offset;
+	}
+
+	if (offset2 >= mainwidth) {
+		 offset2 =  offset;
+	}
+	offset1 += 1;
+	offset2 += 1;
+	$sky1.css("background-position", offset1 + "px 0")
+	
+	$sky2.css("background-position", offset2 + "px 0")
+}, 70);
+
+ // $(function (){
+ //    // var $width=$(".section").width();
+ //    // while(1){
+	//     $(".section").animate({
+	//         "background-position-x": "+1500",
+	//         "background-position-y": "0"
+	//     }, 50000);
+	// 	 $("#sky").animate({
+	//         "background-position-x": "+1500",
+	//         "background-position-y": "0"
+	//     }, 70320);
+
+ // 		// $(".section").css(background-position);
+	// // }
+	//   });
+  });
